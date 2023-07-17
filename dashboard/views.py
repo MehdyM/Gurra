3dab7a14482681743d784df3a055f266f00f459c
Mehdy Moussavi <mehdymoussavi@hotmail.com>
2023-07-17 20:24:18 +0200

    first

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from . import logics 
 
 def home(request):
-    balance = logics.account_balance_for_view()
     content = {
-        'balance': balance[0]
+        'balance': 100
     }
     return render(request, 'dashboard/home.html', content)
