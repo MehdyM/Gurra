db8bdcd7a8487d010807d8d51a62788feeec4761
Mehdy Moussavi <mehdymoussavi@hotmail.com>
2023-07-18 03:27:08 +0200

    v1

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,12 @@ from . import logics
 
 def home(request):
     balance = logics.account_balance_for_view()
+    checks = logics.checks_for_view()
+    sales = logics.sold_for_view()
     content = {
-        'balance': balance[1]
+        'balance': balance[0],
+        'total': balance[1],
+        'checks':checks,
+        'sales':sales
     }
     return render(request, 'dashboard/home.html', content)
